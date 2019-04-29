7463f58c1db7651b3a0b9f85ad3532cada8211d0
User1 <User1@LP-CNU3099WJB.HCLT.CORP.HCL.IN>
2019-04-29 18:20:53 +0530

    update tests cases

--- src/main/java/com/hcl/pp/models/PetModel.java
+++ src/main/java/com/hcl/pp/models/PetModel.java
@@ -9,8 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-
 @Entity
 @Table(name="PETS")
 public class PetModel {
